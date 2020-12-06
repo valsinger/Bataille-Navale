74ea14b823b84cbd1927c2f3f821883c42028293
aurerere <aure.dumay@gmail.com>
2020-12-06 18:53:51 +0100

    update attaque
    spaces and missing something

--- attaque.py
+++ attaque.py
@@ -1,1 +1,35 @@
-from tab import *from boats import *def attaqueUser(x,y):    if m[x][y]=="▤":        torpilleurBot.setPv(torpilleurBot.getPV-20)        m[x][y]=="x"        p[x][y]=="x"        print("touché",x,y)    elif m[x][y]=="▥":        sousmarinBot.setPv(sousmarinBot.getPV-20)        m[x][y]=="x"        p[x][y]=="x"        print("touché",x,y)    elif m[x][y]=="▦":        contretorpilleurBot.setPv(contretorpilleurBot.getPV-20)        m[x][y]=="x"        p[x][y]=="x"        print("touché",x,y)    elif m[x][y]=="▧":        croiseurBot.setPv(croieurBot.getPV-20)        m[x][y]=="x"        p[x][y]=="x"        print("touché",x,y)    elif m[x][y]=="▨":        porteavionsBot.setPv(porteavionsBot.getPV-20)        m[x][y]=="x"        p[x][y]=="x"        print("touché",x,y)    elif m[x][y]=="x":        return False    elif m[x][y]=="~":        m[x][y]=="o"        p[x][y]=="o"        print("dans l'eau")                         
+from tab import *
+from boats import *
+
+def attaqueUser(x,y):
+    if m[x][y] == "▤":
+        torpilleurBot.setPv(torpilleurBot.getPV-20)
+        m[x][y] == "x"
+        p[x][y] = "x"
+        print("touché",x,y)
+    elif m[x][y] == "▥":
+        sousmarinBot.setPv(sousmarinBot.getPV-20)
+        m[x][y] = "x"
+        p[x][y] = "x"
+        print("touché",x,y)
+    elif m[x][y] == "▦":
+        contretorpilleurBot.setPv(contretorpilleurBot.getPV-20)
+        m[x][y] = "x"
+        p[x][y] = "x"
+        print("touché",x,y)
+    elif m[x][y] == "▧":
+        croiseurBot.setPv(croieurBot.getPV-20)
+        m[x][y] = "x"
+        p[x][y] = "x"
+        print("touché",x,y)
+    elif m[x][y] == "▨":
+        porteavionsBot.setPv(porteavionsBot.getPV-20)
+        m[x][y] = "x"
+        p[x][y] = "x"
+        print("touché",x,y)
+    elif m[x][y] == "x" or m[x][y] == "o":
+        return False
+    elif m[x][y] == "~":
+        m[x][y] = "o"
+        p[x][y] = "o"
+        print("dans l'eau")                         
