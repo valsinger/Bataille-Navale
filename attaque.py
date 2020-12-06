5197c23a612a7db18fa5c5ac35eca0fe8482384d
aurerere <aure.dumay@gmail.com>
2020-12-06 18:55:23 +0100

    ggg
    ff

--- attaque.py
+++ attaque.py
@@ -6,30 +6,30 @@ def attaqueUser(x,y):
         torpilleurBot.setPv(torpilleurBot.getPV-20)
         m[x][y] == "x"
         p[x][y] = "x"
-        print("touché",x,y)
+        print("touché")
     elif m[x][y] == "▥":
         sousmarinBot.setPv(sousmarinBot.getPV-20)
         m[x][y] = "x"
         p[x][y] = "x"
-        print("touché",x,y)
+        print("touché")
     elif m[x][y] == "▦":
         contretorpilleurBot.setPv(contretorpilleurBot.getPV-20)
         m[x][y] = "x"
         p[x][y] = "x"
-        print("touché",x,y)
+        print("touché!")
     elif m[x][y] == "▧":
         croiseurBot.setPv(croieurBot.getPV-20)
         m[x][y] = "x"
         p[x][y] = "x"
-        print("touché",x,y)
+        print("touché!")
     elif m[x][y] == "▨":
         porteavionsBot.setPv(porteavionsBot.getPV-20)
         m[x][y] = "x"
         p[x][y] = "x"
-        print("touché",x,y)
+        print("touché!")
     elif m[x][y] == "x" or m[x][y] == "o":
         return False
     elif m[x][y] == "~":
         m[x][y] = "o"
         p[x][y] = "o"
-        print("dans l'eau")                         
+        print("Dans l'eau!")          
