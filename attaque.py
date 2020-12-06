66d1df574182d42ad578d1d2624e1ac6f17c3ed3
valsinger <74239348+valsinger@users.noreply.github.com>
2020-12-06 18:48:46 +0100

    Update attaque.py

--- attaque.py
+++ attaque.py
@@ -1,10 +1,1 @@
-from tab import *from boats import *
-def attaqueUser(x,y):
-    if m[x][y]=="■":
-        m[x][y]=="▢"
-        p[x][y]=="▢"
-        print("touché",x,y)
-        elif m[x][y]=="▢":
-            return False
-            elif m[x][y]=="~":
-                print("dans l'eau")          
+from tab import *from boats import *def attaqueUser(x,y):    if m[x][y]=="▤":        torpilleurBot.setPv(torpilleurBot.getPV-20)        m[x][y]=="x"        p[x][y]=="x"        print("touché",x,y)    elif m[x][y]=="▥":        sousmarinBot.setPv(sousmarinBot.getPV-20)        m[x][y]=="x"        p[x][y]=="x"        print("touché",x,y)    elif m[x][y]=="▦":        contretorpilleurBot.setPv(contretorpilleurBot.getPV-20)        m[x][y]=="x"        p[x][y]=="x"        print("touché",x,y)    elif m[x][y]=="▧":        croiseurBot.setPv(croieurBot.getPV-20)        m[x][y]=="x"        p[x][y]=="x"        print("touché",x,y)    elif m[x][y]=="▨":        porteavionsBot.setPv(porteavionsBot.getPV-20)        m[x][y]=="x"        p[x][y]=="x"        print("touché",x,y)    elif m[x][y]=="x":        return False    elif m[x][y]=="~":        m[x][y]=="o"        p[x][y]=="o"        print("dans l'eau")                         
