e73ec385700c96f3cea64e1efe985a261ab79c50
valsinger <74239348+valsinger@users.noreply.github.com>
2020-12-06 19:27:30 +0100

    attaque la bonne version

--- attaque.py
+++ attaque.py
@@ -10,28 +10,18 @@ def attaqueUser(x,y):
         else:
             torpilleurBot.coule()
             print("coulé")
-        torpilleurBot.setPv(torpilleurBot.getPV-20)
-
         m[x][y] = "x"
-
         p[x][y] = "x"
-
         print("touché")
-
     elif m[x][y] == "▥":
         if sousmarinBot.pv!=0:
             sousmarinBot.setPv(sousmarinBot.getPV-20)
         else:
             sousmarinBot.coule()
-            print("coulé")
-        sousmarinBot.setPv(sousmarinBot.getPV-20)
-
+            print("coulé")       
         m[x][y] = "x"
-
         p[x][y] = "x"
-
         print("touché")
-
     elif m[x][y] == "▦":
         if contretorpilleurBot.pv!=0:
             contretorpilleurBot.setPv(contretorpilleurBot.getPV-20)
@@ -39,49 +29,31 @@ def attaqueUser(x,y):
             contretorpilleurBot.coule()
             print("coulé")
         m[x][y] = "x"
-
         p[x][y] = "x"
-
         print("touché!")
-
     elif m[x][y] == "▧":
         if croiseurBot.pv!=0:
             croiseurBot.setPv(croiseurBot.getPV-20)
         else:
             croiseurBot.coule()
             print("coulé")
-        croiseurBot.setPv(croieurBot.getPV-20)
-
         m[x][y] = "x"
-
         p[x][y] = "x"
-
         print("touché!")
-
     elif m[x][y] == "▨":
         if porteavionsBot.pv!=0:
             porteavionsBot.setPv(porteavionsBot.getPV-20)
         else:
             porteavionsBot.coule()
-            print("coulé")
-        porteavionsBot.setPv(porteavionsBot.getPV-20)
-
+            print("coulé")        
         m[x][y] = "x"
-
         p[x][y] = "x"
-
         print("touché!")
-
     elif m[x][y] == "x" or m[x][y] == "o":
-
         return False
-
     elif m[x][y] == "~":
-
         m[x][y] = "o"
-
         p[x][y] = "o"
-
         print("Dans l'eau!")
          
  
